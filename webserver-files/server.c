56d3c478d66cdf2c5c68f40adf25061911dff229
student <dambiexs@gmail.com>
2024-03-17 12:25:32 +0000

    Implement multi-threading skeleton

--- webserver-files/server.c
+++ webserver-files/server.c
@@ -1,6 +1,12 @@
 #include "segel.h"
 #include "request.h"
 #define ARG_MAX_LEN 10
+#define SAFETY_MARGIN 10
+#define NULL_REQUEST -1
+#define END_OF_BUFFER -2
+int *requests_buffer;
+pthread_cond_t requests_buffer_cond;
+
 
 // 
 // server.c: A very, very simple web server
@@ -12,7 +18,6 @@
 // Most of the work is done within routines written in request.c
 //
 
-// HW3: Parse the new arguments too
 void getargs(int *port, int *num_threads, int *queue_size, char **sched_alg, int argc, char *argv[])
 {
     if (argc < 2) {
@@ -24,11 +29,34 @@ void getargs(int *port, int *num_threads, int *queue_size, char **sched_alg, int
     *queue_size = atoi(argv[3]);
     *sched_alg = argv[4];
 }
-int create_worker_threads();
-// {
-//         pthread_t thread1;
-//     pthread_create(&thread1, NULL, worker_routine, args);
-// }
+
+void worker_routine()
+{
+    int connfd;
+    while (1){
+        connfd = wait_for_request();
+        requestHandle(connfd);
+        Close(connfd);
+    }
+}
+
+int create_worker_threads(int port, int num_threads, int queue_size, char *argv)
+{
+    pthread_t *threads;
+    for (size_t i = 0; i < num_threads; i++)
+    {
+        pthread_create(&threads[i], NULL, worker_routine, argv);
+    }
+    return 0;
+}
+
+initialize_buffer(int size, int* buffer)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        buffer[i] = NULL_REQUEST;
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -39,11 +67,9 @@ int main(int argc, char *argv[])
     char sched_alg[ARG_MAX_LEN];
     getargs(&port, &num_threads, &queue_size, &sched_alg, argc, argv);
 
-    // 
-    // HW3: Create some threads...
-    //
-    create_worker_threads();
-
+    requests_buffer = malloc(queue_size+SAFETY_MARGIN);
+    initialize_buffer(queue_size, requests_buffer);
+    create_worker_threads(port, num_threads, queue_size, argv);
 
     listenfd = Open_listenfd(port);
     while (1) {
@@ -55,11 +81,11 @@ int main(int argc, char *argv[])
 	// Save the relevant info in a buffer and have one of the worker threads 
 	// do the work. 
 	// 
-	requestHandle(connfd);
+	// requestHandle(connfd);
 
-	Close(connfd);
+	// Close(connfd);
     }
-
+    free (requests_buffer);
 }
 
 
